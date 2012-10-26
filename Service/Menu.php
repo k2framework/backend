60f7fbb72cb1b3cdea366f0421f053795c8ce5dc
Manuel Aguirre <programador.manuel@gmail.com>
2012-10-26 14:51:49 -0430

    se crea un primer servicio llamado k2_backend_menu
    El cual renderizará el menú del backend y la app

---
+++ Service/Menu.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace K2\Backend\Service;
+
+use KumbiaPHP\Security\Security;
+
+/**
+ * Description of Menu
+ *
+ * @author maguirre
+ */
+class Menu
+{
+
+    public function __construct(Security $s)
+    {
+        //var_dump();
+    }
+
+}
