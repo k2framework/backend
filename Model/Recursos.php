b37bf6563473dd749fbe791d0566b6416bb29aa2
Manuel Aguirre <programador.manuel@gmail.com>
2012-10-27 15:49:32 -0430

    implementando interfaces necesarias para el backend y el ACL.

--- Model/Recursos.php
+++ Model/Recursos.php
@@ -3,13 +3,19 @@
 namespace K2\Backend\Model;
 
 use KumbiaPHP\ActiveRecord\ActiveRecord;
+use KumbiaPHP\Security\Acl\Resource\ResourceInterface;
 
 /**
  * Description of Recursos
  *
  * @author maguirre
  */
-class Recursos extends ActiveRecord
+class Recursos extends ActiveRecord implements ResourceInterface
 {
-    
+
+    public function getName()
+    {
+        return $this->recurso;
+    }
+
 }
--- Model/Roles.php
+++ Model/Roles.php
@@ -3,13 +3,30 @@
 namespace K2\Backend\Model;
 
 use KumbiaPHP\ActiveRecord\ActiveRecord;
+use KumbiaPHP\Security\Acl\Role\RoleInterface;
 
 /**
  * Description of Roles
  *
  * @author maguirre
  */
-class Roles extends ActiveRecord
+class Roles extends ActiveRecord implements RoleInterface
 {
-    //put your code here
+
+    protected function createRelations()
+    {
+        $this->hasAndBelongsToMany('K2\\Backend\\Model\\Recursos'
+                , 'K2\\Backend\\Model\\RolesRecursos');
+    }
+
+    public function getName()
+    {
+        return $this->rol;
+    }
+
+    public function getResources()
+    {
+        return $this->get('K2\\Backend\\Model\\Recursos');
+    }
+
 }
