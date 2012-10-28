7506eeb10273f0d65429ee4a64aeee39394033c5
Manuel Aguirre <programador.manuel@gmail.com>
2012-10-28 18:20:22 -0430

    reescribiendo el servicio flash...
    Para que los mensajes tengan el diseño del bootstrap

---
+++ Service/Flash.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace K2\Backend\Service;
+
+use KumbiaPHP\Flash\Flash as Base;
+
+/**
+ * Clase que permite el envio de mensajes flash desde un controlador,
+ * para luego ser leido en las vistas.
+ * 
+ * Cada vez que leemos los mensajes que han sido previamente guardados,
+ * estos son borrados de la sesión, para que solo nos aparescan una vez.
+ *
+ * @author manuel
+ */
+class Flash extends Base
+{
+
+    public function __toString()
+    {
+        $code = '<div class="messages-flash">' . PHP_EOL;
+        foreach ((array) $this->getAll() as $type => $message) {
+            $code.= "<div class=\"alert alert-$type\">
+            <button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button>
+            $message</div>" . PHP_EOL;
+        }
+        $code .= '</div>' . PHP_EOL;
+        return $code;
+    }
+
+}
