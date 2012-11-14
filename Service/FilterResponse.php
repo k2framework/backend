b5b9544e43d27a1d692b9ff301a2e011224f69ed
Manuel Aguirre <programador.manuel@gmail.com>
2012-11-14 16:40:13 -0430

    ahora se usa el servicio FilterResponse que hace varias cosas a la vez

---
+++ Service/FilterResponse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace K2\Backend\Service;
+
+use KumbiaPHP\View\View;
+use KumbiaPHP\Kernel\Event\ResponseEvent;
+
+/**
+ * La clase Ejecuta funcionalidades en el evento kumbia.response
+ *
+ * @author manuel
+ */
+class FilterResponse
+{
+
+    public function onResponse(ResponseEvent $event)
+    {
+        if ($event->getRequest()->isAjax() &&
+                ($event->getResponse()->headers->has('Location'))) {
+            //cuando la petición sea ajax y estemos redirigiendo, lo hacemos por javascript.
+            $url = $event->getResponse()->headers->get('Location');
+            $script = "<script>window.location='$url';</script>";
+            die($script);
+            $event->getResponse()->setContent($script);
+            $event->stopPropagation();
+        } elseif ('' === $event->getResponse()->getContent()) {
+            //si no devolvimos ninguna data en la respuesta, cargamos los mensajes flash
+            $event->getResponse()->setContent(View::content(true)); //mostramos los mensajes flash
+        }
+    }
+
+}
