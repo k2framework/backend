c668d18eb657ee81df6101023b9fc07c8df4654e
Manuel Aguirre <programador.manuel@gmail.com>
2012-11-17 19:58:45 -0430

    Ya se pueden editar los privilegios para los roles...

--- Controller/PrivilegiosController.php
+++ Controller/PrivilegiosController.php
@@ -2,9 +2,10 @@
 
 namespace K2\Backend\Controller;
 
-use K2\Backend\Controller\Controller;
-use K2\Backend\Model\Recursos;
 use K2\Backend\Model\Roles;
+use K2\Backend\Model\Recursos;
+use K2\Backend\Model\RolesRecursos;
+use K2\Backend\Controller\Controller;
 
 /**
  * Description of IndexController
@@ -31,15 +32,28 @@ class PrivilegiosController extends Controller
             $this->renderNotFound("No existe el rol con id $rol_id");
         }
 
-        Recursos::createQuery()
-                ->leftJoin('roles_recursos as rr', 'rr.recursos_id = recursos.id')
-                ->columns("recursos.*, rr.roles_id as rol");
-        $this->recursos = Recursos::paginate($pagina);
+        RolesRecursos::createQuery()
+                ->rightJoin('recursos as r', 'r.id = roles_recursos.recursos_id 
+                    AND roles_id = :rol')
+                ->columns("roles_recursos.id,roles_recursos.roles_id,
+                    r.descripcion,r.recurso,r.id as recursos_id")
+                ->group('r.id')
+                ->bindValue('rol', $this->rol->id);
+
+        $this->recursos = RolesRecursos::paginate($pagina);
+        $this->get("k2_debug")->dump("data", $this->recursos);
 
         if ($this->getRequest()->isMethod('post')) {
             $seleccionados = $this->getRequest()->get('recursos');
             //acá debemos actualizar los privilegios del rol.
-            return $this->toAction("editar/{$rol_id}/{$pagina}");
+            if (RolesRecursos::editar($this->rol, $this->recursos->items, $seleccionados)) {
+                $this->get('flash')->success("Los privilegios del Rol 
+                        <b>{$this->rol->getName()}</b> fueron actualizados...!!!");
+                //redireccionamos par que se vuelva a ejecutar la consulta.
+                return $this->toAction("editar/{$rol_id}/{$pagina}");
+            } else {
+                $this->get('flash')->error("No se pudieron Hacer los Cambios...!!!");
+            }
         }
     }
 
--- Model/RolesRecursos.php
+++ Model/RolesRecursos.php
@@ -2,6 +2,7 @@
 
 namespace K2\Backend\Model;
 
+use K2\Backend\Model\Roles;
 use KumbiaPHP\ActiveRecord\ActiveRecord;
 
 /**
@@ -11,5 +12,75 @@ use KumbiaPHP\ActiveRecord\ActiveRecord;
  */
 class RolesRecursos extends ActiveRecord
 {
-    //put your code here
+
+    /**
+     * Actualiza los privilegios para Un Rol en especifico
+     * @param array $privilegios arreglo con los privilegios
+     * @param array $seleccionados arreglo con los id seleccionados.
+     */
+    public static function editar(Roles $rol, array $privilegios, array $seleccionados)
+    {
+        $eliminar = array();
+        $existentes = array();
+        //obtenemos todos los privilegios y los recorremos
+        //vamos verificando si se ha deseleccionado un elemento 
+        //ó aun sigue seleccionado
+        //y vamos llenando un arreglo con los deseleccionados ($eliminar) para eliminarlos de la BD
+        //y otro arreglo con los existentes ($existentes) para no agregarlos a la BD
+        foreach ($privilegios as $re) {
+            if (null === $re->id) {
+                continue;
+            } elseif (!$re->isSelected($rol, $seleccionados)) {
+                //si el recurso no está seleccionado, lo vamos a eliminar
+                $eliminar[] = $re->id;
+            } else {
+                $existentes[] = $re->id;
+            }
+        }
+        //los elementos a agregar son los seleccionados que no existen aun en la BD
+        $agregar = array_diff($seleccionados, $existentes);
+        try {
+            \ActiveRecord\Adapter\Adapter::factory()->pdo()->beginTransaction();
+            //eliminamos los elementos deseleccionados
+            self::deleteByIds($eliminar);
+            $model = new static();
+            foreach ($agregar as $id) {
+                $model->roles_id = $rol->id;
+                $model->recursos_id = (int) $id;
+                if (!$model->create()) {
+                    return false;
+                }
+            }
+        } catch (ActiveRecord\Exception\SqlException $e) {
+            \ActiveRecord\Adapter\Adapter::factory()->pdo()->rollBack();
+            return false;
+        }
+        \ActiveRecord\Adapter\Adapter::factory()->pdo()->commit();
+        return true;
+    }
+
+    /**
+     * Verifica que el objeto aparesca en el arreglo de ids seleccionados.
+     * @param Roles $rol
+     * @param array $selecteds arreglo de ids, ejemplo: array(2,4,67,100,...);
+     * @return booblean 
+     */
+    public function isSelected(Roles $rol, array $selecteds)
+    {
+        return $this->roles_id === $rol->id && in_array($this->recursos_id, $selecteds);
+    }
+
+    public static function deleteByIds(array $ids)
+    {
+        if (count($ids)) {
+            $query = self::createQuery();
+            foreach ($ids as $id) {
+                $id = (int) $id;
+                $query->whereOr("id = :id_{$id}")
+                        ->bindValue("id_{$id}", $id);
+            }
+            self::deleteAll();
+        }
+    }
+
 }
