2288b056bc2fae20696a181ce302d09deccbc69c
root <root@dhcppc0.unassigned>
2012-11-30 22:27:07 +0000

    Se verifica la existencia del error de logueo en la sesión, en el IndexController

--- Controller/indexController.php
+++ Controller/indexController.php
@@ -2,6 +2,7 @@
 
 namespace K2\Backend\Controller;
 
+use KumbiaPHP\Security\Security;
 use KumbiaPHP\Kernel\Controller\Controller;
 
 /**
@@ -9,27 +10,26 @@ use KumbiaPHP\Kernel\Controller\Controller;
  *
  * @author manuel
  */
-class indexController extends Controller
-{
+class indexController extends Controller {
 
-    protected function beforeFilter()
-    {
+    protected function beforeFilter() {
         if ($this->getRequest()->isAjax()) {
             $this->setTemplate(NULL);
-        }else{
-            $this->setTemplate('K2/Backend:login');            
+        } else {
+            $this->setTemplate('K2/Backend:login');
         }
         $this->limitParams = false;
     }
 
-    public function index_action()
-    {
+    public function index_action() {
         return $this->getRouter()->forward('K2/Backend:usuarios/index');
     }
 
-    public function login_action()
-    {
+    public function login_action() {
         $this->setView(null);
+        if ($this->get('session')->has(Security::LOGIN_ERROR)) {
+            $this->get('flash')->error($this->get('session')->get(Security::LOGIN_ERROR));
+        }
     }
 
 }
