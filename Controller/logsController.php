30a252635f818f0cbb4ebbd51c0caaaf143cc446
Manuel Aguirre <programador.manuel@gmail.com>
2013-05-24 16:08:07 -0430

    creando un filtro para los logs

--- Controller/logsController.php
+++ Controller/logsController.php
@@ -3,13 +3,23 @@
 namespace K2\Backend\Controller;
 
 use K2\Backend\Model\Logs;
+use K2\Backend\Model\Usuarios;
 use K2\Backend\Controller\Controller;
+use ActiveRecord\Event\Event;
 
 class logsController extends Controller
 {
 
+    public $tiposConsulta = array(
+        Event::INSERT => Event::INSERT,
+        Event::UPDATE => Event::UPDATE,
+        Event::DELETE => Event::DELETE,
+    );
+
     public function index_action($page = 1)
     {
+        $this->usuarios = Usuarios::findAll(array('activo' => true));
+
         Logs::createQuery()
                 ->columns('logs.*,usuarios.login')
                 ->join('usuarios', 'usuarios.id = usuarios_id')
