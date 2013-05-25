dd46baa82ce6c1df7647e15da923f12fa3d426b4
manuel_j555 <programador.manuel@gmail.com>
2013-05-25 14:16:59 -0430

    la verificación de si la petición es ajax se pasa al beforeFilter

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -13,21 +13,14 @@ use K2\Kernel\Controller\Controller as Base;
 class Controller extends Base
 {
 
-    /**
-     * Luego de ejecutar las acciones, se verifica si la petición es ajax
-     * para no mostrar ni vista ni template.
-     */
-    protected function afterFilter()
-    {
-        if (App::getRequest()->isAjax()) {
-            $this->setView(false);
-        }
-    }
-
     protected function beforeFilter()
     {
         if (!App::get('security')->isLogged()) {
             return App::get('firewall')->showLogin();
+        }
+
+        if (App::getRequest()->isAjax()) {
+            $this->setView(false);
         }
     }
 
